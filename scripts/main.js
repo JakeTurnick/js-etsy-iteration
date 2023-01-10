dc376af7edf0c809c8bf5304051e7c74c14f21ab
JakeTurnick <jaketurnick@gmail.com>
2023-01-10 14:32:20 -0500

    completed #4 'made of wood' and #5 'eight or more materials'

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 
     //Calculates average price, to nearest 100th, for all items in USD
     function average(itemsArr) {
+        console.log('\nAverage price: ')
         let sum = 0;
         let avg = 0;
         itemsArr.forEach(item => {
@@ -18,6 +19,7 @@
 
     //Get array of items between $14 - $18
     function costBetween(itemsArr, min, max) {
+        console.log('\nPrice between range:')
         let validItems = [];
         validItems = itemsArr.filter(item => {
             if (item.price >= min && item.price <= max) {
@@ -32,14 +34,42 @@
 
     //find item with GBP currency_code - print its name and price
     function findBrits(itemsArr) {
+        console.log('\nGBP item:')
         let itemsGBP = [];
         itemsArr.forEach(item => {
             if (item.currency_code === 'GBP') {
                 itemsGBP.push(item);
-                console.log(item.title, '- price:', item.price.toFixed(2));
+                console.log(item.title, '- price:', item.price.toFixed(2), item.currency_code);
             }
         })
         // console.log('GBP items:', itemsGBP)
     }
     findBrits(items);
+
+    //All items made of wood
+    function findWoods(itemsArr) {
+        console.log('\nItems made of wood')
+        let woods = [];
+        itemsArr.forEach(item => {
+            if (item.materials.includes("wood")) {
+                woods.push(item);
+                console.log('item:', item.title, ' is made of ', item.materials)
+            }
+        })
+        console.log(woods)
+    }
+    findWoods(items);
+
+
+    //find items made of 8 or more materials
+    function eightPlus(itemsArr) {
+        console.log('\nItems with 8 or more materials')
+        let overEight = [];
+        overEight = itemsArr.filter(item => item.materials.length >= 8);
+        overEight.forEach(item => console.log('item -', item.title, 'is made of:', item.materials))
+        console.log(overEight)
+    }
+    eightPlus(items);
+
+    
 })();
