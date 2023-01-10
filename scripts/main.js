58997be2188ae5c93dc2b2531e435e18b6897b08
JakeTurnick <jaketurnick@gmail.com>
2023-01-10 13:58:45 -0500

    #2 and #3 completed

--- scripts/main.js
+++ scripts/main.js
@@ -17,5 +17,29 @@
 
 
     //Get array of items between $14 - $18
+    function costBetween(itemsArr, min, max) {
+        let validItems = [];
+        validItems = itemsArr.filter(item => {
+            if (item.price >= min && item.price <= max) {
+                console.log('price:',item.price, ' of -', item.title)
+                return item;
+            }
+        })
+        console.log(validItems);
+    }
+    costBetween(items, 14, 18)
 
+
+    //find item with GBP currency_code - print its name and price
+    function findBrits(itemsArr) {
+        let itemsGBP = [];
+        itemsArr.forEach(item => {
+            if (item.currency_code === 'GBP') {
+                itemsGBP.push(item);
+                console.log(item.title, '- price:', item.price.toFixed(2));
+            }
+        })
+        // console.log('GBP items:', itemsGBP)
+    }
+    findBrits(items);
 })();
